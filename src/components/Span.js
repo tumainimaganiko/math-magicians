d898eff24d4f090587a4334b1bad2aa2ac99c23a
tumainimaganiko <tumainimaganiko@gmail.com>
2023-05-31 18:07:27 +0300

    Add event listeners

--- src/components/Span.js
+++ src/components/Span.js
@@ -1,12 +1,29 @@
 import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
-export default function Span({ text, color }) {
+export default function Span({
+  text, color, obj, setObj,
+}) {
+  function handleClick(buttonName) {
+    setObj(calculate(obj, buttonName));
+  }
   return (
-    <span className={color}>{text}</span>
+    <button onClick={() => handleClick(text)} type="button" className={color}>
+      {text}
+    </button>
   );
 }
 
 Span.propTypes = {
   text: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
+  obj: PropTypes.oneOfType([
+    PropTypes.shape({
+      total: PropTypes.string,
+      next: PropTypes.string,
+      operation: PropTypes.string,
+    }),
+    PropTypes.oneOf([null]),
+  ]).isRequired,
+  setObj: PropTypes.func.isRequired,
 };
