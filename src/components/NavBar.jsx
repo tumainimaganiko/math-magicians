79ebe7024f284aad9c45fb495bcf8dce293b20fd
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-07 06:07:57 +0300

    Create Navigation Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    { path: '/', text: 'Home' },
+    { path: 'quotes', text: 'Quotes' },
+    { path: 'calculator', text: 'Calculator' },
+  ];
+  return (
+
+    <nav className="navbar">
+      <h1>Math Magicians</h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.text}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+
+  );
+};
+export default Navbar;
