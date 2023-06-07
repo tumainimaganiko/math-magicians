86a02464dd0a63d5878b4519bddb6a28a30b935c
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-07 06:08:48 +0300

    Create Home page Component

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+const Home = () => (
+  <div>
+    <h2>Welcome to Our Page!</h2>
+    <div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
+        itaque aspernatur eligendi optio a veniam sapiente deserunt totam
+        assumenda aut, ab nemo suscipit ratione repudiandae doloremque
+        distinctio vitae reiciendis! Dolor sit voluptatem ullam necessitatibus
+        ducimus iure voluptates natus ad aspernatur? Quia quae voluptatibus
+        neque repellat quos magnam suscipit, corporis fugit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus,
+        eligendi aperiam aspernatur, animi vero optio vitae consequuntur
+        aliquam explicabo facere quasi odio qui officiis, repellat et.
+        Eligendi nam debitis iure maiores placeat provident suscipit itaque
+        blanditiis eum! Quisquam ad praesentium tenetur, voluptatem quasi
+        aliquid recusandae aut fuga repellendus ut atque sed corrupti
+        laudantium voluptates omnis molestiae sint deleniti. Doloribus cumque
+        molestias nemo maxime sunt repellendus accusantium repudiandae omnis
+        nihil numquam fugit dolor quis praesentium quo non voluptates
+        voluptatem, vel optio! Sed perferendis libero officia, amet ut minima
+        illum, id sit voluptates quo nemo deserunt, blanditiis quidem
+        reprehenderit! Laudantium, delectus temporibus!
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
